d538764e5489bdafabb5946724bdfdf51e69213e
Rachelle Herrera <rachelle.herera@gmail.com>
2016-03-12 19:39:52 -0800

    changed the port to listen for heroku's port or to default to 3000.

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ app.use(methodOverride('_method'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-/*app.listen(process.env.PORT || 3000)*/;
+app.listen(process.env.PORT || 3000);
 
 // auth middleware
 app.use(require('express-session')({
